93086cc8111b593e5fc1ff05dd488f654994ca0a
vova <chubenko-vv@ukr.net>
2016-06-01 14:14:38 +0300

    next step

---
+++ src/test/java/bms/notpages/LoginTest.java
@@ -0,0 +1,49 @@
+package bms.notpages;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.*;
+import org.testng.annotations.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class LoginTest extends bms.TestBase {
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Test
+  public void testUntitled() throws Exception {
+    driver.get(baseUrl+ "/osop/LoginForm.csp");
+    driver.findElement(By.name("username")).clear();
+    driver.findElement(By.name("username")).sendKeys("vova");
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("bMS$2016");
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
